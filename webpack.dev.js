941c900ec6362f72609786a574bcc60793ea6718
Felix Stubner <alpha2twin@gmail.com>
2021-11-14 00:58:17 +0000

    chore: separated webpack environments into prod and dev

---
+++ webpack.dev.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: '/node_modules/',
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebPackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html'
+        })
+    ],
+    stats: {
+        nestedModules: true,
+        dependentModules: true,
+        groupModulesByPath: true
+    }
+}
