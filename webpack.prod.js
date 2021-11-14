40031e0991799251886abc78593f0e498b95c5fb
Felix Stubner <felix.stubner@gmail.com>
2021-11-14 15:11:53 +0000

    chore: added production optimisations and service workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
 const HTMLWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -15,15 +19,27 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader']
-            }
+            },
+            {
+                test: /.s?css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
         ]
     },
     plugins: [
         new HTMLWebPackPlugin({
             template: './src/client/views/index.html',
             filename: './index.html'
-        })
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
+        new WorkboxPlugin.GenerateSW()
     ],
+    optimization: {
+        minimizer: [
+            new TerserPlugin(),
+            new CssMinimizerPlugin(),
+        ],
+    },
     stats: {
         nestedModules: true,
         dependentModules: true,
